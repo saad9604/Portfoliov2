4424fa578a3b0708506af6e189b56d2429f7be0c
Saad <sayedsaad9604@gmail.com>
2023-10-04 21:53:49 +0530

    icontap fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-around pt-6  text-white">
       <div className="Nav lg:text-[3rem] text-[2rem] font-bold">Saad</div>
-      <div onClick={() => setmenu(!menu)} className="lg:hidden text-[2rem] "><HiMenu/></div>
+      <div className="lg:hidden text-[2rem] "><HiMenu onClick={() => setmenu(!menu)} /></div>
       {
         menu && <div className="flex flex-col justify-around absolute bg-blue-200 text-black rounded-md text-center translate-x-6 translate-y-[115px] z-10 font-extrabold  w-[200px] h-[200px]">
           <ul>About</ul>
