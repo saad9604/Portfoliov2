a06c112afe468c08240250c86966fd920c1e3e67
Saad <sayedsaad9604@gmail.com>
2023-10-04 21:32:36 +0530

    intro fixed

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -44,40 +44,41 @@ const Intro = () => {
               <div className="lg:w-[400px]">
                 <Lottie animationData={anime2} />
               </div>
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                animate={controls}
+                className="lg:w-auto"
+              >
+                <p className="text-center lg:mt-0 mt-7 lg:translate-y-0 translate-y-5 font-extrabold lg:text-[32px] text-[25px] text-blue-500">
+                  Full Stack Development
+                </p>
+                <div className="flex lg:gap-5 gap-3 mx-auto lg:mx-0 lg:h-[50px] h-[40px] mt-10 mb-9 -translate-x-3">
+                  <img src={html} className="shadow-md shadow-black rounded-full p-1" alt="HTML" />
+                  <img src={css} className="shadow-md shadow-black rounded-full p-1" alt="CSS" />
+                  <img src={javascript} className="shadow-md shadow-black rounded-full p-1" alt="JavaScript" />
+                  <img src={react} className="shadow-md shadow-black rounded-full p-1" alt="React" />
+                  <img src={mongodb} className="shadow-md shadow-black rounded-full p-1" alt="MongoDB" />
+                  <img src={node} className="shadow-md shadow-black rounded-full p-1" alt="Node.js" />
+                  <img src={prisma} className="shadow-md shadow-black rounded-full p-1" alt="Prisma" />
+                </div>
+                <div className="flex flex-col lg:pt-0 pt-5">
+                  <div className="flex gap-1 font-extrabold mb-2 text-blue-500">
+                    <img src={light} width={20} alt="Light Bulb" />
+                    <p>Building responsive Single-Page-Apps (SPA) in React.js</p>
+                  </div>
+                  <div className="flex gap-1 font-extrabold mb-2 text-blue-500">
+                    <img src={light} width={20} alt="Light Bulb" />
+                    <p>Building responsive static websites using MERN Stack</p>
+                  </div>
+                  <div className="flex gap-1 font-extrabold mb-2 text-blue-500">
+                    <img src={light} width={20} alt="Light Bulb" />
+                    <p>Building RESTful APIs using Express.js and Node.js</p>
+                  </div>
+                </div>
+              </motion.div>
           </div>
          
-            <motion.div
-              initial={{ opacity: 0, y: 50 }}
-              animate={controls}
-              className="lg:w-auto"
-            >
-              <p className="text-center lg:mt-0 mt-7 lg:translate-y-0 translate-y-5 font-extrabold lg:text-[32px] text-[25px] text-blue-500">
-                Full Stack Development
-              </p>
-              <div className="flex lg:gap-5 gap-3 mx-auto lg:mx-0 lg:h-[50px] h-[40px] mt-10 mb-9 -translate-x-3">
-                <img src={html} className="shadow-md shadow-black rounded-full p-1" alt="HTML" />
-                <img src={css} className="shadow-md shadow-black rounded-full p-1" alt="CSS" />
-                <img src={javascript} className="shadow-md shadow-black rounded-full p-1" alt="JavaScript" />
-                <img src={react} className="shadow-md shadow-black rounded-full p-1" alt="React" />
-                <img src={mongodb} className="shadow-md shadow-black rounded-full p-1" alt="MongoDB" />
-                <img src={node} className="shadow-md shadow-black rounded-full p-1" alt="Node.js" />
-                <img src={prisma} className="shadow-md shadow-black rounded-full p-1" alt="Prisma" />
-              </div>
-              <div className="flex flex-col lg:pt-0 pt-5">
-                <div className="flex gap-1 font-extrabold mb-2 text-blue-500">
-                  <img src={light} width={20} alt="Light Bulb" />
-                  <p>Building responsive Single-Page-Apps (SPA) in React.js</p>
-                </div>
-                <div className="flex gap-1 font-extrabold mb-2 text-blue-500">
-                  <img src={light} width={20} alt="Light Bulb" />
-                  <p>Building responsive static websites using MERN Stack</p>
-                </div>
-                <div className="flex gap-1 font-extrabold mb-2 text-blue-500">
-                  <img src={light} width={20} alt="Light Bulb" />
-                  <p>Building RESTful APIs using Express.js and Node.js</p>
-                </div>
-              </div>
-            </motion.div>
+            
           </motion.div>
         </div>
       )}
