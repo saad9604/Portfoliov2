4b9a9df515bc68aaaefeafafe927a891f48fc95b
Saad <sayedsaad9604@gmail.com>
2023-10-08 21:00:51 +0530

    devdetective issue solved

--- src/components/ProjectCard2.js
+++ src/components/ProjectCard2.js
@@ -21,8 +21,8 @@ const ProjectCard2 = ({ project }) => {
                 onSlideChange={() => console.log('slide change')}
             >
                 {projectsData2.map((project) => (
-                    <div className=''>
-                        <SwiperSlide className=''>
+                    <div className='mx-auto'>
+                        <SwiperSlide>
                             <div className='flex items-center lg:justify-center justify-between max-w-[90%] mx-auto lg:pb-0 pb-2'>
                                 <p className='lg:text-center  lg:ml-0  name text-[1rem] mb-2 font-bold'>{project.title}</p>
                                 <div className='lg:hidden flex'>
@@ -34,15 +34,15 @@ const ProjectCard2 = ({ project }) => {
                                     </a>
                                 </div>
                             </div>
-                            <div className='mx-auto min-w-[350px] max-w-[350px] border-[2px] rounded-3xl'>
-                                <img src={project.image} alt={project.title} width={350} height={200} className='max-h-[300px] rounded-3xl min-h-[300px] object-cover shadow-blue-500 shadow-xl' />
+                            <div className='mx-auto min-w-[350px] max-w-[350px] border-[2px] rounded-3xl lg:-translate-x-16'>
+                                <img src={project.image} alt={project.title} width={350} height={200} className='max-h-[300px] rounded-3xl lg:min-w-[500px] min-h-[300px]  object-fill shadow-blue-500 shadow-xl' />
                             </div>
-                            <div className='lg:flex hidden lg:flex-col justify-center bottom-[360px] left-6 lg:p-0 p-5  items-center  relative lg:left-[18%] gap-3 lg:bottom-[250px]'>
+                            <div className='lg:flex hidden lg:flex-row justify-center bottom-[360px] left-6 lg:p-5 p-5  items-center  relative lg:left-[7.4%] gap-3 lg:bottom-[357px] z-30'>
                                 <a target='_blank' href={project.link}>
-                                    <img src={project.live} alt='Live' width={40} height={10}/>
+                                    <img src={project.live} alt='Live' className='lg:w-7'/>
                                 </a>
                                 <a target='_blank' href={project.gitLink}>
-                                    <img src={project.github} alt='GitHub' width={40} height={10} />
+                                    <img src={project.github} alt='GitHub' className='lg:w-7' />
                                 </a>
                             </div>
                         </SwiperSlide>
